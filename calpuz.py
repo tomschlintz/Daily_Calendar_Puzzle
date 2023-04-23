f61ed098a4a640b4cebf40d3b1b952d1b228310b
Tom Gordon <tom@barefootsoftwareconsulting.com>
2023-04-23 10:02:37 -0400

    Add board object, establish pieces

--- calpuz.py
+++ calpuz.py
@@ -1,6 +1,99 @@
 #!/usr/bin/python3
 
 import sys
+from datetime import datetime
+
+##
+ # Define a "board" object, which represents places to put all the pieces.
+ # Any spots on the board that are not placeable are set to 1.
+ # The spots on the board for the month and day are set to 1.
+ # All other spots are left at zero.
+ # Placing a piece on the board adds it's pattern for each spot it covers,
+ # and any spot > 1 represents an overlap, either with unplaceable spots,
+ # OR with already placed pieces.
+ ##
+class Board:
+    def __init__(self, date):
+        self.width = 7
+        self.height = 7
+        self.month = datetime.month
+        self.date = datetime.day
+
+        # Represent board as 2D array.
+        self.rows = [[0]*self.height for i in range(self.width)]
+
+        # Establish unusable spots on the board.
+        self.rows[0][6] = 1
+        self.rows[1][6] = 1
+        self.rows[6][3] = 1
+        self.rows[6][4] = 1
+        self.rows[6][5] = 1
+        self.rows[6][6] = 1
+
+        # Mark spots on board for date given, as these should not be covered.
+        self.setDate(date)
+
+    # Mark spots on board for month and day that can't be covered.
+    def setDate(self, date):
+        m = date.month - 1  # get 0-based month {0..11}
+        d = date.day - 1    # get 0-based day of month {0..20}
+        self.rows[int(m / 12)][int(m % 12)] = 1
+        self.rows[int(2 + d/7)][int(d % 7)] = 1
+
+    ##
+     # Place a piece on the board.
+     # The piece rotation is already established before passing it to this method, so we don't worry about that here.
+     # \param piece piece object to be placed
+     # \param x linear location - this is 0 at (0,0), incrementing across each column, then down each row
+     # \returns True if valid - fits in board and does not overlap any invalid spot or other piece already placed
+     ##
+    def place(self, piece, x):
+        # Copy board, in case we fail to place the part.
+        brdCopy = self.rows
+
+        # Derive coord of piece upper-left corner from linear location
+        x0, y0 = self.coordFromLinear(x)
+
+        # Invalid if piece rectangle goes outside of board rectangle. This should be true regardless
+        # of the actual shape of the piece.
+        return False if (x0 + piece.width) >= self.width or (y0 + piece.height) >= self.height else None
+    
+        # Superimpose piece onto board, by simpy adding 1 to overlapping spots.
+        for y in range(len(rows)):
+            for x in range(len(rows[0])):
+                self.rows[x0+x][y0+y] += piece.rows[x][y]
+    
+        # Invalid if any part of the piece overlaps either board spots that cannot be covered, or any other piece.
+        if not self.isBoardValid():
+            self.rows = brdCopy
+            return False
+
+    ##
+     # Scans all board spots, and return True only if no overlaps are detected.
+     ##
+    def isBoardValid(self):
+        for r in self.rows:
+            for c in r:
+                return False if c > 1 else None
+        return True
+
+    def dump(self):
+        print('{} wide'.format(self.width))
+        print('{} high'.format(self.height))
+        print('Pattern:')
+        for r in self.rows:
+            for c in r:
+                if c:
+                    sys.stdout.write('1')
+                else:
+                    sys.stdout.write('0')
+            sys.stdout.write('\n')
+
+    ##
+     # Get (x,y) of 2D board array from linear increment.
+     ##
+    def coordFromLinear(self, x):
+        return x / self.width, x % self.width
 
 class Piece:
     def __init__(self, rows):
@@ -8,14 +101,15 @@ class Piece:
         self.width = len(rows[0])
         self.height = len(rows)
         self.rows = rows
+        self.rotation = 0   # Track current rotation for the piece
 
     def rotate(self):
         # Create new rows, where width is height, heith is width.
-        newRows = [[None]*self.height for i in range(self.width)]
+        newRows = [[0]*self.height for i in range(self.width)]
         # newRows = [[None,None],[None,None],[None,None]]
 
         # No transpose, rotating CCW.
-        spot = False
+        spot = 0
         for nrnum in range(self.width):
             for ncnum in range(self.height):
                 c = self.width - nrnum - 1
@@ -26,6 +120,7 @@ class Piece:
         newWidth = self.height
         self.height = self.width
         self.width = newWidth
+        self.rotation = (self.rotation + 1) % 4     # 0=none, 1=CCW once, 2=CCW twice, 3=CCW thrice
 
     def dump(self):
         print('{} wide'.format(self.width))
@@ -34,12 +129,31 @@ class Piece:
         for r in self.rows:
             for c in r:
                 if c:
-                    sys.stdout.write('X')
+                    sys.stdout.write('1')
                 else:
-                    sys.stdout.write(' ')
+                    sys.stdout.write('0')
             sys.stdout.write('\n')
 
 def main():
+
+    # Establish all pieces used. Initial orientation for each is arbitrary.
+    piece = \
+        [ \
+            Piece([[1,0,1],[1,1,1]]), \
+            Piece([[0,0,1],[1,1,1],[1,0,0]]), \
+            Piece([[1,1,1],[1,0,0],[1,0,0]]), \
+            Piece([[0,0,1,1],[1,1,1,0]]), \
+            Piece([[1,1,1,1],[1,0,0,0]]), \
+            Piece([[1,1,1,1],[0,0,1,0]]), \
+            Piece([[1,1,1],[0,1,1]]), \
+            Piece([[1,1,1],[1,1,1]]), \
+        ]
+
+    # brd = Board(datetime.now())
+    # brd.dump()
+    for p in piece:
+        p.dump()
+    return
 
     # a = [[0,1,2],[10,11,12]]
     # a[1][1] = 5
@@ -47,7 +161,13 @@ def main():
     # return
 
     print('Working..')
-    p = Piece([[True, False, True], [True, True, True]])
+    p = Piece([[1, 0, 1], [1, 1, 1]])
+    p.dump()
+    p.rotate()
+    p.dump()
+    p.rotate()
+    p.dump()
+    p.rotate()
     p.dump()
     p.rotate()
     p.dump()
