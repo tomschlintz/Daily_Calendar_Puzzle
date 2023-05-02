5f354af3a1031bd41e259d0a5a75f85b8ada5e64
Tom Gordon <tom@barefootsoftwareconsulting.com>
2023-05-02 17:40:47 -0400

    Add utility script to try a range of dates.
    Change-Id: I5272bbc4862d232fac86d2522cc487ff2312983d

---
+++ z.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import calpuz
+from datetime import datetime
+import calendar
+
+YEAR = 2024
+
+def main():
+
+    for m in range(1, 13):
+        lastDayInMonth = calendar.monthrange(2024, m)[1]
+        for d in range(1, lastDayInMonth+1):
+            dstr = '{}/{}/{}'.format(m, d, YEAR)
+            sys.argv = ['calpuz.py', dstr, 'gonogo']
+            calpuz.main()
+
+if __name__ == "__main__":
+    main()
